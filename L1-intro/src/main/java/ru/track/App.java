37cbcc787091826743056cba50aaba25ff7e01a9
guraleks <aleksei.gurov@phystech.edu>
2018-02-25 20:25:49 +0300

    homework intro

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,5 +1,10 @@
 package ru.track;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONException;
+
 /**
  * TASK:
  * POST request to  https://guarded-mesa-31536.herokuapp.com/track
@@ -15,17 +20,25 @@ public class App {
     public static final String FIELD_NAME = "name";
     public static final String FIELD_GITHUB = "github";
     public static final String FIELD_EMAIL = "email";
+    public static boolean success = false;
 
     public static void main(String[] args) throws Exception {
-       // 1) Use Unirest.post()
+        // 1) Use Unirest.post()
         // 2) Get response .asJson()
         // 3) Get json body and JsonObject
         // 4) Get field "success" from JsonObject
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                .field(FIELD_NAME, "Aleksey")
+                .field(FIELD_GITHUB, "guraleks")
+                .field(FIELD_EMAIL, "gurov.aleksei@yandex.ru")
+                .asJson();
 
+        try {
+            success = (Boolean) jsonResponse.getBody().getObject().get("success");
+        } catch (JSONException e) {
+        }
 
-
-
-        boolean success = false;
+//        System.out.println(success);
     }
 
 }
